16daeda81d3559dc9385ce90deabd8a03601e383
Illia Chernyk <ilyachernykan@gmail.com>
2023-07-09 18:06:10 +0300

    Edit function

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const userInputValue = prompt('Введіть хоча б один символ'
 function getUserInputValue() {
     if (userInputValue === null || userInputValue === '') {
         return 'try again';
-        
-    } else {
-        return userInputValue;
-    }
+    } 
+    return userInputValue;
 }
 console.log(getUserInputValue());
 
